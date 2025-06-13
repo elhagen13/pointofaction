e5714be79fc4af0bfb676fb82f586a64665680a6
Ella Hagen <elhagen@calpoly.edu>
2025-06-13 11:12:07 -0700

    Update images.js

--- poa/src/app/gallery/images.js
+++ poa/src/app/gallery/images.js
@@ -1,161 +1,161 @@
 const images = [
     {
-      link: "/sample_works/image1.png",
+      link: "/sample_works/image1.jpeg",
       type: "Embroidery",
       company: "Company Name 1"
     },
     {
-      link:  "/sample_works/image2.png",
+      link:  "/sample_works/image2.jpeg",
       type: "Embroidery",
       company: "Company Name 2"
 
     },
     {
-      link:  "/sample_works/image3.png",
+      link:  "/sample_works/image3.jpeg",
       type: "Embroidery",
       company: "Company Name 3"
 
     },
     {
-      link:  "/sample_works/image4.png",
+      link:  "/sample_works/image4.jpeg",
       type: "Embroidery",
       company: "Company Name 4"
 
     },
     {
-      link:  "/sample_works/image5.png",
+      link:  "/sample_works/image5.jpeg",
       type: "Vinyl",
       company: "Company Name 5"
 
     },
     {
-      link:  "/sample_works/image6.png",
+      link:  "/sample_works/image6.jpeg",
       type: "LaserEtching",
       company: "Company Name 6"
 
     },
     {
-      link:  "/sample_works/image7.png",
+      link:  "/sample_works/image7.jpeg",
       type: "LaserEtching",
       company: "Company Name 7"
 
     },
     {
-      link:  "/sample_works/image9.png",
+      link:  "/sample_works/image9.jpeg",
       type: "DirectToFilm",
       company: "Company Name 8"
 
     },
     {
-      link:  "/sample_works/image9.png",
+      link:  "/sample_works/image9.jpeg",
       type: "Vinyl",
       company: "Company Name 9"
 
     },
     {
-      link:  "/sample_works/image10.png",
+      link:  "/sample_works/image10.jpeg",
       type: "Vinyl",
       company: "Company Name 10"
 
     },
     {
-      link:  "/sample_works/image11.png",
+      link:  "/sample_works/image11.jpeg",
       type: "Vinyl",
       company: "Company Name 11"
 
     }, 
     {
-      link:  "/sample_works/image12.png",
+      link:  "/sample_works/image12.jpeg",
       type: "DirectToFilm",
       company: "Company Name 12"
 
     },
     {
-      link:  "/sample_works/image13.png",
+      link:  "/sample_works/image13.jpeg",
       type: "Vinyl",
       company: "Company Name 13"
 
     },
     {
-      link:  "/sample_works/image14.png",
+      link:  "/sample_works/image14.jpeg",
       type: "Vinyl",
       company: "Company Name 14"
 
     },
     {
-      link:  "/sample_works/image15.png",
+      link:  "/sample_works/image15.jpeg",
       type: "Vinyl",
       company: "Company Name 15"
 
     },
     {
-      link:  "/sample_works/image16.png",
+      link:  "/sample_works/image16.jpeg",
       type: "LaserEtching",
       company: "Company Name 16"
 
     },
     {
-      link:  "/sample_works/image17.png",
+      link:  "/sample_works/image17.jpeg",
       type: "LaserEtching",
       company: "Company Name 17"
 
     },
     {
-      link:  "/sample_works/image18.png",
+      link:  "/sample_works/image18.jpeg",
       type: "LaserEtching",
       company: "Company Name 18"
 
     },
     {
-      link:  "/sample_works/image19.png",
+      link:  "/sample_works/image19.jpeg",
       type: "Embroidery",
       company: "Company Name 19"
 
     },
     {
-      link:  "/sample_works/image20.png",
+      link:  "/sample_works/image20.jpeg",
       type: "Embroidery",
       company: "Company Name 20"
 
     },
     {
-      link:  "/sample_works/image21.png",
+      link:  "/sample_works/image21.jpeg",
       type: "Embroidery",
       company: "Company Name 21"
 
     },
     {
-      link:  "/sample_works/image22.png",
+      link:  "/sample_works/image22.jpeg",
       type: "Patches",
       company: "Company Name 22"
 
     },
     {
-      link:  "/sample_works/image23.png",
+      link:  "/sample_works/image23.jpeg",
       type: "Patches",
       company: "Company Name 23"
 
     },
     {
-      link:  "/sample_works/image24.png",
+      link:  "/sample_works/image24.jpeg",
       type: "Patches",
       company: "Company Name 24"
 
     },
     {
-      link:  "/sample_works/image25.png",
+      link:  "/sample_works/image25.jpeg",
       type: "Patches",
       company: "Company Name 25"
 
     },
     {
-      link:  "/sample_works/image26.png",
+      link:  "/sample_works/image26.jpeg",
       type: "Patches",
       company: "Company Name 26"
 
     },
     {
-      link:  "/sample_works/image27.png",
+      link:  "/sample_works/image27.jpeg",
       type: "Patches",
       company: "Company Name 27"
 
