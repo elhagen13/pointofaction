6716d2004c3576556a0deb2fca1d3c50393e3e2a
elhagen13 <113931989+elhagen13@users.noreply.github.com>
2025-09-29 11:44:07 -0700

    update

--- poa/src/app/api/inventory/tracker/route.js
+++ poa/src/app/api/inventory/tracker/route.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 // MongoDB connection string - replace with your actual connection string
 const MONGODB_URI = process.env.MONGO_URI;
-const DATABASE_NAME = "testing";
+const DATABASE_NAME = "test";
 const COLLECTION_NAME = "trackedInventory";
 
 
