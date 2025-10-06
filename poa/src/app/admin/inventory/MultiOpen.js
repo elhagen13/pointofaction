ab14e572e7b16cf6b4a366164c3b0f1af71141f0
elhagen13 <113931989+elhagen13@users.noreply.github.com>
2025-10-06 11:43:08 -0700

    fix view (2)

--- poa/src/app/admin/inventory/MultiOpen.js
+++ poa/src/app/admin/inventory/MultiOpen.js
@@ -102,7 +102,9 @@ export default function MultiOpen({
         >
           
           {items.map((item, index) => {
-            item.quantity > 0 && (item.boxId ? boxTotals[item.boxId] > 0 : true) && <div
+            return(
+              item.quantity > 0 && (item.boxId ? boxTotals[item.boxId] > 0 : true) &&
+            <div
               key={item.id || index} // Use item.id if available, otherwise index
               onClick={() => {
                 onClose();
@@ -138,7 +140,7 @@ export default function MultiOpen({
                 Box Total: {item.boxId ? (boxTotals[item.boxId] ?? "Loading...") : "N/A"}
               </h4>
             </div>
-          })}
+          )})}
         </div>
       </div>
     </div>
